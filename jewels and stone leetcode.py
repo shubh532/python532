c63d3adf3071b08bf14e22bb847dba95e06fb72a
shubh532 <125346653+shubh532@users.noreply.github.com>
2023-02-14 13:17:29 +0530

    upload python file
    jewels and stone

---
+++ jewels and stone leetcode.py
@@ -0,0 +1,17 @@
+jewels="aA"
+stones="aAAbbbb"
+ans=0
+"""for i in jewels:
+    for j in stones:
+        if i==j:
+            ans+=1
+
+print(ans)"""
+
+u=set()
+
+for i in jewels:
+    u.add(i)
+for i in stones:
+    if
+    
